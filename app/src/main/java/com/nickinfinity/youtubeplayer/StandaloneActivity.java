3547e87bb01c372e653c1c20d40aaeebd92e11ce
Nickinfinity <nikhilg.me@gmail.com>
2021-01-19 00:33:00 +0530

    Add StandAlone Activity

---
+++ app/src/main/java/com/nickinfinity/youtubeplayer/StandaloneActivity.java
@@ -0,0 +1,49 @@
+package com.nickinfinity.youtubeplayer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+public class StandaloneActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standalone);
+
+
+        Button btnPlayVideo = findViewById(R.id.btnPlayVideo);
+        Button btnPlayList = findViewById(R.id.btnPlayList);
+
+        //Just another way to call on clickListener
+        btnPlayVideo.setOnClickListener(this);
+        btnPlayList.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.btnPlayVideo:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, YoutubeActivity.GOOGLE_API_KEY, YoutubeActivity.YT_VIDEO_ID);
+                break;
+            case R.id.btnPlayList:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, YoutubeActivity.GOOGLE_API_KEY, YoutubeActivity.YT_PLAYLIST);
+                break;
+
+            default:
+
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+}
