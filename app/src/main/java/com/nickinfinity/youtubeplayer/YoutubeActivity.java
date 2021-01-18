ed363230152b5f10ba898473d2d20565d3733156
Nickinfinity <nikhilg.me@gmail.com>
2021-01-18 22:52:13 +0530

    YoutubeActivity: Adding Keys

--- app/src/main/java/com/nickinfinity/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/nickinfinity/youtubeplayer/YoutubeActivity.java
@@ -13,6 +13,9 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
         implements YouTubePlayer.OnInitializedListener {
+    static final String GOOGLE_API_KEY = "AIzaSyC3L0kFoJ_GJ08Sojpbf2RUh4r3UnjIxcc";
+    static final String YT_VIDEO_ID = "M7XM597XO94";
+    static final String YT_PLAYLIST = "PLJ7A97yl9oXqruomJvjLeeK-8hvjUL4V2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,11 @@ public class YoutubeActivity extends YouTubeBaseActivity
 
         // Inflating views using java code instead of xml
         // Dynamically
-        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube,null);
+        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
         setContentView(layout);
 
         YouTubePlayerView player = new YouTubePlayerView(this); // passing the context in every dynamically created view is imp
-        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
         // adding the Youtube Player View to layout file
         layout.addView(player);
