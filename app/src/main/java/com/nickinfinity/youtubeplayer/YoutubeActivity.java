249cba47b78e3df838b1c9e8e519adf75ce690cd
Nickinfinity <nikhilg.me@gmail.com>
2021-01-18 22:25:28 +0530

    YoutubeActivity: Add YoutubePlayerView Dynamically

--- app/src/main/java/com/nickinfinity/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/nickinfinity/youtubeplayer/YoutubeActivity.java
@@ -1,10 +1,14 @@
 package com.nickinfinity.youtubeplayer;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 
 public class YoutubeActivity extends YouTubeBaseActivity
@@ -13,12 +17,22 @@ public class YoutubeActivity extends YouTubeBaseActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+
+        // Inflating views using java code instead of xml
+        // Dynamically
+        ConstraintLayout layout = getLayoutInflater().inflate(R.layout.activity_youtube);
+        setContentView(layout);
+
+        YouTubePlayerView player = new YouTubePlayerView(this); // passing the context in every dynamically created view is imp
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+
+        // adding the Yuutube Player View to layout file
+        layout.addView(player);
     }
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-        
+
     }
 
     @Override
